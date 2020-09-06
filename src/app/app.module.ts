103f5a61737dbe839da5e6c8af372b6ffd2773da
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2020-09-06 09:47:35 -0400

    Setting up Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,25 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-  declarations: [
-	AppComponent,
-	HeaderComponent,
-	RecipesComponent,
-	RecipeListComponent,
-	RecipeDetailComponent,
-	RecipeItemComponent,
-	ShoppingListComponent,
-	ShoppingEditComponent, 
-	DropdownDirective
-  ],
-  imports: [
-	BrowserModule,
-	FormsModule
-  ],
-  providers: [ShoppingListService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		RecipesComponent,
+		RecipeListComponent,
+		RecipeDetailComponent,
+		RecipeItemComponent,
+		ShoppingListComponent,
+		ShoppingEditComponent,
+		DropdownDirective
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		AppRoutingModule
+	],
+	providers: [ShoppingListService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
