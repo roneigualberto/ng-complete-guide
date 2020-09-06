13e205b54893929650f3cb3c0f7efdea0ea2cf35
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2020-09-06 09:47:50 -0400

    Adding Navigation to the App

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,10 +8,7 @@ import  { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
 
-	@Output() featureSelected = new EventEmitter<string>();
+	
 
-	onSelect(feature: string): void {
-		this.featureSelected.emit(feature);
-	}
-
+	
 }
