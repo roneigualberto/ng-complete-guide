7de6607713a3d53f01a7cb8d213ed0e630f0e01a
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2020-09-11 20:57:53 -0400

    Improving the Reactive Service with Observables

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,35 +1,30 @@
-import {Ingredient} from  '../shared/ingredients.model'
-import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
+
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
+  ingredientsChanged = new Subject<Ingredient[]>();
 
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
-	ingredientsChanged = new EventEmitter<Ingredient[]>();
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
 
-	private ingredients:Ingredient[] =  [
-		new Ingredient('Apples', 5),
-		new Ingredient('Tomatoes', 10)
-	];
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 
-
-	getIngredients(): Ingredient[] {
-		return this.ingredients.slice();
-	}
-
-	addIngredient(ingredient: Ingredient) {
-		this.ingredients.push(ingredient);
-		this.ingredientsChanged.emit(this.ingredients.slice());
-	}
-
-	addIngredients(ingredients: Ingredient[]) {
-		/*for(let ingredient of ingredients) {
+  addIngredients(ingredients: Ingredient[]) {
+    /*for(let ingredient of ingredients) {
 			this.addIngredient(ingredient);
 		}*/
 
-		this.ingredients.push(...ingredients);
-		this.ingredientsChanged.emit(this.ingredients.slice());
-	}
-
-
-	
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
