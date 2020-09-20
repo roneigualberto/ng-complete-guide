9c6b2fd6a5c3e8ec4f6866c183f0ef096f47d427
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2020-09-20 09:27:06 -0400

    Preparing the Signup Request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  WEB_API_KEY = 'AIzaSyAe3j_Ox0Z2xL_ONtwF9-isf63YEFYsb8A';
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://www.googleapis.com/identitytoolkit/v3/relyimportparty/signupNewUser?key=${this.WEB_API_KEY}`,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      }
+    );
+  }
+}
